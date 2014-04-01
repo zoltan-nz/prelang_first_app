5865f5afa3fff321d452ad1473f292494cb9f13b
Prelang Builder <builder@prelang.com>
2014-04-01 21:04:44 +0000

    Modified the Devise initializer to add :login as an authentication key

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -254,4 +254,5 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
   config.omniauth :facebook, "", ENV['FACEBOOK_SECRET_ID']
+  config.authentication_keys = [:login]
 end
